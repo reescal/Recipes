e490c8425a5733c0d12b7d9c255bd7be9aee7ad3
reescal <robertoescalera@outlook.com>
2022-08-05 18:55:11 -0400

    Create Ingredient service and interface. Gets and returns string from api. Register service. Install extension for named http client. Install extension for DI. Inject ingredients service to index page. Get string from ingredient service.

--- src/Recipes.Web/Program.cs
+++ src/Recipes.Web/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Recipes.Web;
+using Recipes.Web.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("API", x => x.BaseAddress = new Uri(builder.Configuration["API"]));
+
+builder.Services.AddScoped<IIngredientsService, IngredientsService>();
 
 await builder.Build().RunAsync();
---
+++ src/Recipes.Web/Services/IngredientsService.cs
@@ -0,0 +1,18 @@
+﻿namespace Recipes.Web.Services;
+
+public class IngredientsService : IIngredientsService
+{
+    private readonly HttpClient _httpClient;
+
+    public IngredientsService(IHttpClientFactory clientFactory)
+    {
+        _httpClient = clientFactory.CreateClient("API");
+    }
+
+    public async Task<string> Get() => await _httpClient.GetStringAsync("api/Ingredients");
+}
+
+public interface IIngredientsService
+{
+    Task<string> Get();
+}
