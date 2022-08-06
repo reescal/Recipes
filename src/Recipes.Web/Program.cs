9ae13078f98f6ec8aec7c2606e95044f0ae7bde1
reescal <robertoescalera@outlook.com>
2022-08-05 23:19:46 -0400

    Uri is host environment base address if read uri is null.

--- src/Recipes.Web/Program.cs
+++ src/Recipes.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddHttpClient("API", x => x.BaseAddress = new Uri(builder.Configuration["API"]));
+builder.Services.AddHttpClient("API", x => x.BaseAddress = new Uri(builder.Configuration["API"] ?? builder.HostEnvironment.BaseAddress));
 
 builder.Services.AddScoped<IIngredientsService, IngredientsService>();
 
