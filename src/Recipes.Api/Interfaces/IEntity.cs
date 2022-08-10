c4a376039d43533a8e380559ec89821c9eae06cb
reescal <robertoescalera@outlook.com>
2022-08-10 13:52:59 -0400

    Create Ingredient model. Create IEntity interface which all entities will implement. Includes Id and Image.

---
+++ src/Recipes.Api/Interfaces/IEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Recipes.Api.Interfaces;
+
+public interface IEntity
+{
+    public Guid Id { get; set; }
+    public string Image { get; set; }
+}
---
+++ src/Recipes.Api/Models/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using Recipes.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Recipes.Api.Models;
+
+public class Ingredient : IngredientCreate, IEntity
+{
+    public Guid Id { get; set; }
+}
+
+public class IngredientCreate
+{
+    public string Image { get; set; }
+    public HashSet<IngredientProperties> Properties { get; set; }
+}
+
+public class IngredientProperties
+{
+    public int LangId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
