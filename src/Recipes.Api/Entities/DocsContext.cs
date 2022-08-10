09470c5ab0abb4e6dfbef34b4bfdde326e207985
reescal <robertoescalera@outlook.com>
2022-08-10 13:51:22 -0400

    Add cosmosdb context factory. Reads connection string from environment variables. Add efcore for cosmosdb.

---
+++ src/Recipes.Api/Entities/DocsContext.cs
@@ -0,0 +1,17 @@
+﻿using Recipes.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Recipes.Api.Entities
+{
+    public class DocsContext : DbContext
+    {
+        public DocsContext(DbContextOptions<DocsContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+    }
+}
---
+++ src/Recipes.Api/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Api.Entities;
+using Recipes.Api;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace Recipes.Api;
+
+public class Startup : FunctionsStartup
+{
+    private static readonly IConfigurationRoot Configuration = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddEnvironmentVariables()
+        .Build();
+
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddDbContextFactory<DocsContext>(
+           (DbContextOptionsBuilder opts) =>
+           {
+               var connectionString = Configuration["CosmosDBConnection"];
+               var databaseName = Configuration["DatabaseName"];
+               if (string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName))
+                   throw new InvalidOperationException(
+                       "Please specify a valid CosmosDB connection string and database name in the local.settings.json file or your Azure Functions Settings.");
+
+               opts.UseCosmos(
+                   connectionString,
+                   databaseName);
+           });
+    }
+}
