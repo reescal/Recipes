66cf1e8b8c232446f56188cf8d0eda61c693466b
reescal <robertoescalera@outlook.com>
2022-08-10 20:00:11 -0400

    Created helper method to deserialize request. Created helper method to determine if lang id input is valid or not and same for a HashSet of Entity properties.

---
+++ src/Recipes.Api/Wrappers/Helpers.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Recipes.Api.Enums;
+using Recipes.Api.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipes.Api.Wrappers;
+
+public static class Helpers
+{
+    public static async Task<T> DeserializeAsync<T>(HttpRequest req)
+    {
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var input = JsonConvert.DeserializeObject<T>(requestBody);
+        return input;
+    }
+
+    public static bool LangExists(int langId) => Enum.IsDefined(typeof(Lang), langId);
+
+    public static bool LangsExist(HashSet<IEntityProperties> properties)
+    {
+        var langs = properties.Select(x => x.LangId);
+        foreach (var lang in langs)
+        {
+            if (!LangExists(lang))
+                return false;
+        }
+        return true;
+    }
+}
