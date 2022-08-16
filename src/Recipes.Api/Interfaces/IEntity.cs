c3f1a33edb8bc8179d38c278f983516cd7f5ecfd
reescal <robertoescalera@outlook.com>
2022-08-16 18:26:09 -0400

    Removed models and interfaces which have been copied over.

--- src/Recipes.Api/Interfaces/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Recipes.Api.Interfaces;
-
-public interface IEntity
-{
-    public Guid Id { get; set; }
-    public string Image { get; set; }
-}
--- src/Recipes.Api/Interfaces/IEntityProperties.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Recipes.Api.Interfaces;
-
-public interface IEntityProperties
-{
-    public int LangId { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-}
