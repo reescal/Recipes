253ba4e840f583da30468226f270963d317c6ac7
reescal <robertoescalera@outlook.com>
2022-08-16 20:14:44 -0400

    Created a couple helper methods. DRY.

--- src/Recipes.Api/Wrappers/Helpers.cs
+++ src/Recipes.Api/Wrappers/Helpers.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Recipes.Shared.Models;
+using static Recipes.Api.Constants.Responses;
 
 namespace Recipes.Api.Wrappers;
 
@@ -21,7 +24,7 @@ public static class Helpers
 
     public static bool LangExists(int langId) => Enum.IsDefined(typeof(Lang), langId);
 
-    public static bool LangsExist(HashSet<IEntityProperties> properties)
+    public static bool LangsExist(IEnumerable<IEntityProperties> properties)
     {
         var langs = properties.Select(x => x.LangId);
         foreach (var lang in langs)
@@ -30,5 +33,35 @@ public static class Helpers
                 return false;
         }
         return true;
+    }
+
+    public static async Task<T> FindById<T>(DbSet<T> ctx, Guid id) where T : class
+    {
+        var result = await ctx.FindAsync(id);
+
+        if (result == null)
+            throw new ApiException(NotFound(nameof(T), id), 404);
+
+        return result;
+    }
+
+    public static IEnumerable<SimpleEntity> FilterLang(this IEnumerable<SimpleEntity> l, int? _lang)
+    {
+        if (_lang == null)
+            return l;
+
+        if (!LangExists((int)_lang))
+            throw new ApiException(InvalidLang((int)_lang), 400);
+
+        l = l.Where(x => x.Properties.Any(y => y.LangId == _lang))
+                            .Select(x => new SimpleEntity()
+                            {
+                                Id = x.Id,
+                                Properties = x.Properties
+                                                .Where(y => y.LangId == _lang)
+                                                .ToHashSet()
+                            });
+
+        return l;
     }
 }
