ddfea4502e406d324cb83c14d3aa77de962b7dde
reescal <robertoescalera@outlook.com>
2022-12-16 14:46:24 -0500

    Create helper for configuration.

---
+++ src/Recipes.Shared/Helpers/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using static Recipes.Shared.Constants.DBConstants;
+
+namespace Recipes.Shared.Helpers;
+public static class Helpers
+{
+    public static class Configuration
+    {
+        public static (string ConnectionString, string DatabaseName) Cosmos(IConfigurationRoot config)
+        {
+            var connectionString = config[connString] ?? config.GetSection("Values").GetValue<string>(connString);
+            var databaseName = config[dbName] ?? config.GetSection("Values").GetValue<string>(dbName);
+            return string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName)
+                ? throw new InvalidOperationException(ex)
+                : ((string ConnectionString, string DatabaseName))(connectionString, databaseName);
+        }
+    }
+}
