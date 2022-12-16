b41d56c21822bb60749f23d2e289c55eed7297e7
reescal <robertoescalera@outlook.com>
2022-12-16 17:24:57 -0500

    Add logging?

--- src/Recipes.Shared/Helpers/Helpers.cs
+++ src/Recipes.Shared/Helpers/Helpers.cs
@@ -10,9 +10,10 @@ public static class Helpers
         public static (string ConnectionString, string DatabaseName, string ContainerName) Cosmos(IConfigurationRoot config)
         {
             var connectionString = config[connString] ?? config.GetSection("Values").GetValue<string>(connString);
-            var databaseName = config[DBConstants.dbName] ?? config.GetSection("Values").GetValue<string>(dbName);
+            var databaseName = config[dbName] ?? config.GetSection("Values").GetValue<string>(dbName);
             var containerName = config[DBConstants.containerName] 
                 ?? config.GetSection("Values").GetValue<string>(DBConstants.containerName);
+            Console.WriteLine($"~!@# Database: {databaseName} ~!@# Container: {containerName}");
             return string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName)
                 ? throw new InvalidOperationException(ex)
                 : ((string ConnectionString, string DatabaseName, string ContainerName))(connectionString, databaseName, containerName);
