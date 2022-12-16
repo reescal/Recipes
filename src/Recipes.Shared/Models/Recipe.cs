75a048a68b44ad915370ae8ef5295de7fc16203d
reescal <robertoescalera@outlook.com>
2022-12-16 14:44:12 -0500

    Complete validators. Use constants instead of hard-coded strings.

--- src/Recipes.Shared/Models/Recipe.cs
+++ src/Recipes.Shared/Models/Recipe.cs
@@ -1,6 +1,8 @@
 ﻿using FluentValidation;
+using Recipes.Shared.Constants;
 using Recipes.Shared.Enums;
 using Recipes.Shared.Interfaces;
+using static Recipes.Shared.Models.ValidatorHelpers.ValidatorHelpers;
 
 namespace Recipes.Shared.Models;
 
@@ -65,12 +67,13 @@ public class RecipeValidator : AbstractValidator<RecipeCreate>
 {
     public RecipeValidator()
     {
-        RuleFor(p => p.Image).NotEmpty().WithMessage("Image link required");
-        RuleFor(p => p.Yield).NotEmpty().WithMessage("Yield required");
-        RuleFor(p => p.Time).NotEmpty().WithMessage("Time required");
-        RuleFor(x => x.Properties).NotNull().WithMessage("Properties required");
+        RuleFor(p => p.Image).NotEmpty().WithMessage(ValidationError.Required("Image link"));
+        RuleFor(p => p.Image).Must(BeUri).When(p => p.Image != null).WithMessage(ValidationError.Invalid("image link"));
+        RuleFor(p => p.Yield).NotEmpty().WithMessage(ValidationError.Required(nameof(RecipeCreate.Yield)));
+        RuleFor(p => p.Time).InclusiveBetween(5, 2880).WithMessage(ValidationError.InclusiveBetween(nameof(RecipeCreate.Time), 5, 2880));
+        RuleFor(x => x.Properties).Must(p => p != null && p.Any()).WithMessage(ValidationError.Required(nameof(RecipeCreate.Properties)));
         RuleForEach(x => x.Properties).SetValidator(new RecipePropertiesValidator());
-        RuleForEach(x => x.Ingredients).NotNull().WithMessage("Ingredients required");
+        RuleFor(x => x.Ingredients).Must(p => p != null && p.Any()).WithMessage(ValidationError.Required(nameof(RecipeCreate.Ingredients)));
         RuleForEach(x => x.Ingredients).SetValidator(new IngredientRowValidator());
     }
 }
@@ -79,10 +82,14 @@ public class RecipePropertiesValidator : AbstractValidator<RecipeProperties>
 {
     public RecipePropertiesValidator()
     {
-        RuleFor(p => p.LangId).IsInEnum().WithMessage("Invalid language");
-        RuleFor(p => p.Name).NotEmpty().WithMessage("Name required");
-        RuleFor(p => p.Description).NotEmpty().WithMessage("Description required");
-        RuleFor(p => p.Type).NotEmpty().WithMessage("Type required");
+        RuleFor(p => p.LangId).IsInEnum().WithMessage(ValidationError.Invalid("Language"));
+        RuleFor(p => p.Name).NotEmpty().WithMessage(ValidationError.Required(nameof(RecipeProperties.Name)));
+        RuleFor(p => p.Name).MinimumLength(3).When(p => !string.IsNullOrEmpty(p.Name)).WithMessage(ValidationError.TooShort(nameof(RecipeProperties.Name)));
+        RuleFor(p => p.Name).MaximumLength(50).When(p => !string.IsNullOrEmpty(p.Name)).WithMessage(ValidationError.TooLong(nameof(RecipeProperties.Name)));
+        RuleFor(p => p.Description).NotEmpty().WithMessage(ValidationError.Required(nameof(RecipeProperties.Description)));
+        RuleFor(p => p.Type).Must(p => !string.IsNullOrEmpty(p)).WithMessage(ValidationError.Required(nameof(RecipeProperties.Type)));
+        RuleFor(p => p.Type).MinimumLength(3).When(p => !string.IsNullOrEmpty(p.Type)).WithMessage(ValidationError.TooShort(nameof(RecipeProperties.Type)));
+        RuleFor(p => p.Type).MaximumLength(50).When(p => !string.IsNullOrEmpty(p.Type)).WithMessage(ValidationError.TooLong(nameof(RecipeProperties.Type)));
     }
 }
 
@@ -90,10 +97,11 @@ public class IngredientRowValidator : AbstractValidator<IngredientRow>
 {
     public IngredientRowValidator()
     {
-        //RuleFor(p => p.IngredientId) DI context and check here???
-        RuleFor(x => x.Quantity).NotNull().WithMessage("Quantity required");
+        //RuleFor(p => p.IngredientId) DI context and check here instead of CheckIngredients method on RecipesService?
+        RuleFor(x => x.Quantity).NotNull().WithMessage(ValidationError.Required(nameof(IngredientRow.Quantity)));
         RuleFor(p => p.Quantity).SetValidator(new QuantityValidator());
-        RuleFor(p => p.Preparation).NotNull().WithMessage("Preparation required");
+        RuleFor(p => p.Preparation).NotEmpty().WithMessage(ValidationError.Required(nameof(IngredientRow.Preparation)));
+        RuleFor(p => p.Preparation).MaximumLength(50).When(p => !string.IsNullOrEmpty(p.Preparation)).WithMessage(ValidationError.TooLong(nameof(IngredientRow.Preparation)));
     }
 }
 
@@ -101,7 +109,8 @@ public class QuantityValidator : AbstractValidator<Quantity>
 {
     public QuantityValidator()
     {
-        RuleFor(p => p.Value).GreaterThan(0).WithMessage("Value required");
-        RuleFor(p => p.Unit).NotEmpty().WithMessage("Unit required");
+        RuleFor(p => p.Value).GreaterThan(0).WithMessage(ValidationError.Required(nameof(Quantity.Value)));
+        RuleFor(p => p.Unit).NotEmpty().WithMessage(ValidationError.Required(nameof(Quantity.Unit)));
+        RuleFor(p => p.Unit).MaximumLength(10).WithMessage(ValidationError.TooLong(nameof(Quantity.Unit)));
     }
 }
---
+++ src/Recipes.Shared/Models/ValidatorHelpers/ValidatorHelpers.cs
@@ -0,0 +1,8 @@
+﻿namespace Recipes.Shared.Models.ValidatorHelpers;
+
+public static class ValidatorHelpers
+{
+    public static bool BeUri(string uri) => uri.StartsWith("https://") && ImageFormats.Contains(uri[uri.LastIndexOf('.')..]);
+
+    private static List<string> ImageFormats => new() { ".png", ".jpg", ".jpeg", ".gif" };
+}
