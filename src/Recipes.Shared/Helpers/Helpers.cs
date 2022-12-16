3b0c60ce579e737d797a563277f5264b940228cb
reescal <robertoescalera@outlook.com>
2022-12-16 17:11:13 -0500

    Startup config is now public so can be set from test proj. DocsContext now registered directly instead of factory. Helper created to create context so it can be used from test proj. DocsContext now takes in container name.

--- src/Recipes.Shared/Helpers/Helpers.cs
+++ src/Recipes.Shared/Helpers/Helpers.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Recipes.Shared.Constants;
 using static Recipes.Shared.Constants.DBConstants;
 
 namespace Recipes.Shared.Helpers;
@@ -6,13 +7,15 @@ public static class Helpers
 {
     public static class Configuration
     {
-        public static (string ConnectionString, string DatabaseName) Cosmos(IConfigurationRoot config)
+        public static (string ConnectionString, string DatabaseName, string ContainerName) Cosmos(IConfigurationRoot config)
         {
             var connectionString = config[connString] ?? config.GetSection("Values").GetValue<string>(connString);
-            var databaseName = config[dbName] ?? config.GetSection("Values").GetValue<string>(dbName);
+            var databaseName = config[DBConstants.dbName] ?? config.GetSection("Values").GetValue<string>(dbName);
+            var containerName = config[DBConstants.containerName] 
+                ?? config.GetSection("Values").GetValue<string>(DBConstants.containerName);
             return string.IsNullOrEmpty(connectionString) || string.IsNullOrEmpty(databaseName)
                 ? throw new InvalidOperationException(ex)
-                : ((string ConnectionString, string DatabaseName))(connectionString, databaseName);
+                : ((string ConnectionString, string DatabaseName, string ContainerName))(connectionString, databaseName, containerName);
         }
     }
 }
